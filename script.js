3a27624c75d13fb2e51367b38bfb8d72cd1d52d0
stavperry <162802041+stavperry@users.noreply.github.com>
2026-04-06 17:38:19 +0100

    event listener mouseover added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gridTarget = document.querySelector("#container");
 
 function mkDiv() {
-    for (i = 0; i < 256; i++) {
+    for (let i = 0; i < 256; i++) {
         const square = document.createElement("div") // creates a div
         square.classList.add("squares")
         gridTarget.appendChild(square);
@@ -9,4 +9,12 @@ function mkDiv() {
 
 }
 
-console.log(mkDiv());
+mkDiv();
+
+// i want to create an eventlistener that changes the div background-colour from gray to black on mouse-over.
+gridTarget.addEventListener("mouseover", (e) => {
+    // e.target refers to the specific .squares div your mouse is over
+    if (e.target.classList.contains("squares")) {
+        e.target.style.backgroundColor = "black";
+    }
+});
