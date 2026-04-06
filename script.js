dad278d5dc133cde275289b5c5dbd0f005b9b4f0
stavperry <162802041+stavperry@users.noreply.github.com>
2026-04-06 19:12:18 +0100

    user input grid size added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function mkDiv() {
     for (let i = 0; i < 256; i++) {
         const square = document.createElement("div") // creates a div
         square.classList.add("squares")
+        square.style.height = `${800 / 16}px`; // default height of 16px
+        square.style.width = `${800 / 16}px`; // default weight of 16px
         gridTarget.appendChild(square);
     }
 
@@ -17,4 +19,23 @@ gridTarget.addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("squares")) {
         e.target.style.backgroundColor = "black";
     }
+});
+
+// use the button on the page to make a pop up to ask for grid size.
+const btn = document.querySelector(".popUp");
+
+//this triggers the pop up that asks for an input from the user regarding grid size.
+btn.addEventListener("click", (e) => {
+    let gridSize = Number(prompt("how many squares per row would you like?"));
+    function newCanvas() {
+        gridTarget.innerHTML = "";
+        for (let i = 0; i < (gridSize * gridSize); i++ ) {
+            const square = document.createElement("div") // creates a div
+            square.classList.add("squares")
+            square.style.height = `${800 / gridSize}px`; // height set to 800 divided by user input number
+            square.style.width = `${800 / gridSize}px`;
+            gridTarget.appendChild(square);        
+    }
+}
+newCanvas();
 });
